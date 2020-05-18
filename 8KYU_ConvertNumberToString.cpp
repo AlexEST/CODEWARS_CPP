ab68b9370dbefac663c5a4dd68507cb9c24613b8
Alexander Chelpanov <achelpanov86@gmail.com>
2020-05-18 21:54:40 +0300

    Add files via upload

---
+++ 8KYU_ConvertNumberToString.cpp
@@ -0,0 +1,27 @@
+//We need a function that can transform a number into a string.
+//What ways of achieving this do you know?
+//
+//Examples:
+//number_to_string(123) // "123"
+//number_to_string(999) // "999"
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+std::string number_to_string(int num) {
+    return to_string(num);
+}
+
+int main() {
+    cout<<number_to_string(123);
+    return 0;
+}
+
+
+
+
+
+
+
+
